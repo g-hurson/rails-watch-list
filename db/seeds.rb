20a048ccb8dc98ecd9fc6de576271b16cb225f76
Geraldine Hurson <geraldinehurson@gmail.com>
2024-05-23 12:41:10 +0200

    --wip-- [skip ci]

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+require "open-uri"
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+
+# Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+# puts "Created Wonder Woman 1984"
+# Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+# puts "Created The Shawshank Redemption"
+# Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+# puts "Created Titanic"
+# Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+# puts "Created Ocean's Eight"
+
+url = "https://tmdb.lewagon.com/movie/popular"
+response_serialized = URI.open(url).read
+response_parsed = JSON.parse(response_serialized)
+
+response_parsed["results"].each do |movie|
+  Movie.create(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w185#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+  puts "Created #{movie["original_title"]}"
+end
+
+puts "Finished!"
